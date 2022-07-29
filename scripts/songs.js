00a92981a66418489144ebb489189c57ccbeeb26
jadenday20 <day18007@byui.edu>
2022-07-29 16:28:54 -0600

    added the sheet music

---
+++ scripts/songs.js
@@ -0,0 +1,33 @@
+const requestURL = 'sheet_music/songs.json';
+
+const cards = document.querySelector("#songs");
+
+function displaySongs(song) {
+    let card = document.createElement('section');
+    let doc = document.createElement('iframe');
+    let subtitle = document.createElement('p');
+
+    doc.setAttribute('src', `sheet_music/${song.file}.pdf`)
+    doc.setAttribute('frameborder', `0`)
+    // doc.setAttribute('onload', `resizeIframe(this)`)
+    // doc.onload = function(){
+    //     doc.style.height = doc.contentWindow.document.documentElement.scrollHeight + 'px';
+    // }
+    card.appendChild(doc);
+    cards.appendChild(card);
+}
+
+// function resizeIframe(frame) {
+//     frame.style.height = frame.contentWindow.document.documentElement.scrollHeight + 'px';
+// }
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const songs = jsonObject['songs'];
+        songs.forEach(displaySongs);
+
+    });

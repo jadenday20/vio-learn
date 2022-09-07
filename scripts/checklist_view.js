fce9f343de2d78fe5d91cdf095a0d222fa56d53d
jadenday20 <day18007@byui.edu>
2022-09-07 17:10:04 -0600

    added the list view and checklist view options

--- scripts/checklist_view.js
+++ scripts/checklist_view.js
@@ -1,20 +1,62 @@
-let assignments = document.querySelector('.assignments');
-let label = document.createElement('label');
-let select = document.createElement('select');
-
-label.setAttribute('for', 'assignment-list')
-select.setAttribute('name', 'assignment-list')
-select.setAttribute('id', 'assignment-list')
-select.setAttribute('multiple', 'true')
-
-assignments.appendChild(label);
-assignments.appendChild(select)
-
-function get_checkboxes(assignment){
-    let text = assignment.textContent;
-    let checkbox = document.createElement('option');
-    checkbox.setAttribute('value', text);
-    assignment.appendChild(checkbox);
+function add_checkboxes(){
+    let assignments = document.querySelectorAll('.assignments > *');
+    let ul = document.querySelector('.assignments')
+    assignments.forEach(get_checkboxes);
+    
+    function get_checkboxes(assignment){
+        if (assignment.tagName === 'LI'){
+            let text = assignment.textContent;
+            let checkbox = document.createElement('input');
+            let label_text = document.createElement('span');
+            checkbox.setAttribute('type', 'checkbox');
+            checkbox.setAttribute('name', text);
+            label_text.textContent = text;
+            let label = document.createElement('label');
+            // label.setAttribute('for', text)
+            ul.appendChild(label);
+            label.appendChild(checkbox);
+            label.appendChild(label_text);
+            ul.appendChild(document.createElement('br'));
+            assignment.remove();
+        }
+        else{
+            const clone = assignment.cloneNode(true);
+            ul.appendChild(clone);
+            assignment.remove();
+        }
+        
+    }
+    let checklist_button = document.querySelector('#checklist_view');
+    checklist_button.style.display = 'none';
+    let list_button = document.querySelector('#list_view');
+    list_button.style.display = 'block';
 }
 
-assignments.forEach(get_checkboxes);
+function remove_checkboxes(){
+    let assignments = document.querySelectorAll('.assignments > *');
+    let ul = document.querySelector('.assignments')
+    assignments.forEach(get_checkboxes);
+    
+    function get_checkboxes(assignment){
+        if (assignment.tagName === 'LABEL'){
+            let text = assignment.textContent;
+            let li = document.createElement('li');
+            li.textContent = text;
+            ul.appendChild(li)
+            assignment.remove();
+        }
+        else if (assignment.tagName === 'BR'){
+            assignment.remove();
+        }
+        else{
+            const clone = assignment.cloneNode(true);
+            ul.appendChild(clone);
+            assignment.remove();
+        }
+        
+    }
+    let checklist_button = document.querySelector('#checklist_view');
+    checklist_button.style.display = 'block';
+    let list_button = document.querySelector('#list_view');
+    list_button.style.display = 'none';
+}
